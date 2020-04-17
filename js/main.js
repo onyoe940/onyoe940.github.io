dff94d192d7fcbca8c3c83d142ab334c9644cfe1
onyoe940 <54233271+onyoe940@users.noreply.github.com>
2020-04-17 21:59:46 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
 
     $('.menu').click(function() {
         $('.menu_content_area').css("opacity","1");
-        $('.menu_content_area').css("visibility","visible")});
+        $('.menu_content_area').css("visibility","visible");
+        $('.top_header').addClass('top_header_mobile')});        
     $('.menu_close').click(function() {
         $('.menu_content_area').css("opacity","0");
-        $('.menu_content_area').css("visibility","hidden")});
-        
+        $('.menu_content_area').css("visibility","hidden")
+        $('.top_header').removeClass('top_header_mobile')});
     $('.menu').hover(function(){
         $('.menu_bar_top').css('top','23px');
         $('.menu_bar_bottom').css('top','13px');
