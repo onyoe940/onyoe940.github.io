5bee5d22dd05d40d669c66e2de3bf113d6ed6e2b
onyoe940 <54233271+onyoe940@users.noreply.github.com>
2020-04-18 00:12:21 +0800

    up

--- js/main.js
+++ js/main.js
@@ -2,13 +2,12 @@ $(document).ready(function(){
 
     $('.menu').click(function() {
         $('.menu_content_area').css("opacity","1");
-        $('.menu_content_area').css("visibility","visible")
-        $('.top_header').css("height","100vh")});        
+        $('.menu_content_area').css("visibility","visible");
+        $('.top_header').addClass('top_header_mobile')});        
     $('.menu_close').click(function() {
         $('.menu_content_area').css("opacity","0");
         $('.menu_content_area').css("visibility","hidden")
-    $('.top_header').css("height","51px")});
-        
+        $('.top_header').removeClass('top_header_mobile')});
     $('.menu').hover(function(){
         $('.menu_bar_top').css('top','23px');
         $('.menu_bar_bottom').css('top','13px');
